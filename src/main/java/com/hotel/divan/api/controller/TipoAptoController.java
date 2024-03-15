fc7a0198661c49b5cfb5993a87b61ab7237a92be
gvan1961 <gvan1961@hotmail.com>
2024-03-14 22:16:59 -0300

    Nova

--- src/main/java/com/hotel/divan/api/controller/TipoAptoController.java
+++ src/main/java/com/hotel/divan/api/controller/TipoAptoController.java
@@ -3,8 +3,13 @@ package com.hotel.divan.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hotel.divan.domain.model.TipoApto;
@@ -15,6 +20,7 @@ import com.hotel.divan.domain.repository.TipoAptoRepository;
 @RequestMapping("/tipoAptos")
 public class TipoAptoController {
 	
+	//private static final HttpStatus value =  = null;
 	@Autowired
 	private TipoAptoRepository tipoAptoRepository;
 
@@ -22,4 +28,18 @@ public class TipoAptoController {
 	public List<TipoApto> listar() {
 		return tipoAptoRepository.listar();
 	}
+	
+	@ResponseStatus(HttpStatus.CREATED)
+	@GetMapping("/{tipoAptoId}")
+	public ResponseEntity<TipoApto> buscar(@PathVariable("tipoAptoId") Long id) {
+		TipoApto tipoApto = tipoAptoRepository.buscar(id);
+		
+		if (tipoApto != null) {
+			return ResponseEntity.ok(tipoApto);
+		}
+		
+		//return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		return ResponseEntity.notFound().build();
+	}	
+		
 }
