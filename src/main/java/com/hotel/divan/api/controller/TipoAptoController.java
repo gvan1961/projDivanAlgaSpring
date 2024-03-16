60ce0a5919b878a0b0e6ff6ebfa9de8d1a6218ab
gvan1961 <gvan1961@hotmail.com>
2024-03-15 21:27:43 -0300

    Put

--- src/main/java/com/hotel/divan/api/controller/TipoAptoController.java
+++ src/main/java/com/hotel/divan/api/controller/TipoAptoController.java
@@ -2,12 +2,15 @@ package com.hotel.divan.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +44,26 @@ public class TipoAptoController {
 		//return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		return ResponseEntity.notFound().build();
 	}	
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public TipoApto adicionar(@RequestBody TipoApto tipoApto) {
+		return tipoAptoRepository.salvar(tipoApto);
+	}
+	
+	@PutMapping("/{tipoAptoId}")
+	public ResponseEntity<TipoApto> atualizar(@PathVariable Long tipoAptoId,
+			@RequestBody TipoApto tipoApto) {
+		TipoApto tipoAptoAtual = tipoAptoRepository.buscar(tipoAptoId);
 		
+		if (tipoAptoAtual != null) {
+//			cozinhaAtual.setNome(cozinha.getNome());
+			BeanUtils.copyProperties(tipoApto, tipoAptoAtual, "id");
+			
+			tipoAptoAtual = tipoAptoRepository.salvar(tipoAptoAtual);
+			return ResponseEntity.ok(tipoAptoAtual);
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
 }
